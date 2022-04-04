ebdacfed113746eb31e255229002f099575844a5
dongbox <sfreebobo@163.com>
2022-04-04 21:44:25 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,31 +1,58 @@
-from django.test import TestCase
+from tkinter.tix import Tree
+from lists.models import Item
+from django.test import Client, TestCase
 from django.urls import resolve
 from lists.views import home_page
-from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-# Create your tests here.
 class SmokeTest(TestCase):
+    
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        # response = home_page(request)
+        response = self.client.get('/', data={'item_text': 'A new list item'})
         expected_html = render_to_string('home.html')
-        print(expected_html)
         self.assertEqual(expected_html, response.content.decode())
-
+        
     def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
+        response = self.client.post('/', data={'item_text': 'A new list item'})
 
-        response = home_page(request)
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        
+    
+    def test_home_page_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
