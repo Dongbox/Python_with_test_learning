3054137656ae2cfd15ce2c5cbd7946dfff9c0a9a
dongbox <sfreebobo@163.com>
2022-04-04 19:40:09 +0800

    update a func to retrieve

--- functional_tests.py
+++ functional_tests.py
@@ -5,9 +5,15 @@ class NewVisitTestCase(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Edge(r'C:\Program Files (x86)\Microsoft\edgedriver_win64\msedgedriver.exe')
         self.browser.implicitly_wait(3)
+
     def tearDown(self):
         self.browser.quit()
-    
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -18,10 +24,9 @@ class NewVisitTestCase(unittest.TestCase):
         
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         self.fail('Finish the test!')
 
 unittest.main(warnings='ignore')
