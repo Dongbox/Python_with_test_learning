73bd0eb83c0313153d5155216bb2e41f3c4c7be5
dongbox <sfreebobo@163.com>
2022-04-04 18:37:21 +0800

    Functional test now check we can input a to-do list

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 # Create your tests here.
 class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/home')
+        found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
--- superlists/urls.py
+++ superlists/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from unicodedata import name
 from django import urls
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
-import lists
+from django.conf.urls import include, url
+
+from lists.views import home_page
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path(r'home', lists.views.home_page),
+    url(r'^$', home_page, name='home'),
 
 ]
