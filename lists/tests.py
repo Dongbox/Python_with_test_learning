d13f4bba78218dd86b0282eb6b6b740f74f9e28d
dongbox <sfreebobo@163.com>
2022-04-04 18:16:19 +0800

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -12,6 +12,6 @@ class SmokeTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpResponse()
         response = home_page(request)
-        self.assertTrue(response.content.startswith('b<html>'))
+        self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith('</html>'))
+        self.assertTrue(response.content.endswith(b'</html>'))
