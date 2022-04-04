121f5d73084ab179efb4d937cab0d51838191c22
dongbox <sfreebobo@163.com>
2022-04-04 18:13:03 +0800

    First unit test amd url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpResponse
+
 # Create your tests here.
 class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/home')
-        print(found)
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpResponse()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith('b<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith('</html>'))
